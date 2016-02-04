13602db5f95d1678001266896c5add3552411199
weritos6666 <weritos94@gmail.com>
2016-02-04 20:22:04 +0500

    Initialization touchscreen ft5306_ts

--- arch/arm/mach-msm/board-qrd7627a_i6.c
+++ arch/arm/mach-msm/board-qrd7627a_i6.c
@@ -65,6 +65,10 @@
 #include "board-msm7627a.h"
 #include "board-msm7627a_i6-sensor.h"
 
+#ifdef CONFIG_TS_FT5306
+#include <linux/ft5306_ts.h>
+#endif
+
 #define RESERVE_KERNEL_EBI1_SIZE	0x3A000
 #define MSM_RESERVE_AUDIO_SIZE	0xF0000
 #define BOOTLOADER_BASE_ADDR    0x10000
@@ -191,6 +195,73 @@ static int msm_ion_camera_size_carving;
 #define CAMERA_HEAP_TYPE	ION_HEAP_TYPE_DMA
 #else
 #define CAMERA_HEAP_TYPE	ION_HEAP_TYPE_CARVEOUT
+#endif
+
+#ifdef CONFIG_TS_FT5306
+
+#define FT5306_I2C_NAME "ft5306_ts"
+#define FT5306_GPIO_IRQ 48
+#define FT5306_GPIO_RST 26
+#define FT5306_I2C_ADDR 0x38
+
+#define MAX_TOUCH_NUM 10
+
+static int ft5306_ts_gpio_init(void)
+{
+	int ret;
+	ret = gpio_request(FT5306_GPIO_IRQ, "ft5306 IRQ GPIO");
+	if (ret) {
+		printk(KERN_ERR "%s Failed to request GPIO %d\n",
+				__func__, FT5306_GPIO_IRQ);
+		return ret;
+	}
+
+	gpio_tlmm_config(GPIO_CFG(FT5306_GPIO_IRQ, 0, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_6MA),GPIO_CFG_ENABLE);
+
+	return 0;
+}
+
+static int ft5306_ts_platform_init(void)
+{
+	int ret;
+	ret = gpio_request(FT5306_GPIO_RST, "ft5306 RST GPIO");
+	if (ret) {
+		printk(KERN_ERR "%s Failed to request GPIO %d\n",
+				__func__, FT5306_GPIO_RST);
+		return ret;
+	}
+
+	gpio_tlmm_config(GPIO_CFG(FT5306_GPIO_RST, 0, GPIO_CFG_OUTPUT, GPIO_CFG_PULL_UP, GPIO_CFG_8MA),GPIO_CFG_ENABLE);
+	gpio_set_value(FT5306_GPIO_RST, 1);
+
+	return 0;
+}
+
+static int ft5306_ts_platform_exit(void)
+{
+	return 0;
+}
+
+static struct ft5306_platform_data ft5306_ts_pdata = {
+	.name = FT5306_I2C_NAME,
+	.numtouch = MAX_TOUCH_NUM,
+	.display_minx = 0,
+	.display_maxx = 540,
+	.display_miny = 0,
+	.display_maxy = 960,
+	.irq_gpio = FT5306_GPIO_IRQ,
+	.init_platform_hw = ft5306_ts_platform_init,
+	.exit_platform_hw = ft5306_ts_platform_exit,
+	.init_gpio = ft5306_ts_gpio_init,
+};
+
+static struct i2c_board_info ft5306_i2c_device[] = {
+	{
+		I2C_BOARD_INFO(FT5306_I2C_NAME, FT5306_I2C_ADDR),
+		.platform_data = &ft5306_ts_pdata,
+	},
+};
+
 #endif
 
 static struct android_usb_platform_data android_usb_pdata = {
@@ -460,13 +531,30 @@ static int __init reserve_adsp_size_setup(char *p)
 
 early_param("reserve_adsp_size", reserve_adsp_size_setup);
 
+static u32 msm_calculate_batt_capacity(u32 current_voltage);
+
 static struct msm_psy_batt_pdata msm_psy_batt_data = {
-	.voltage_min_design     = 3200,
-	.voltage_max_design     = 4200,
-	.voltage_fail_safe      = 3298,
+	.voltage_min_design     = 2800,
+	.voltage_max_design     = 4300,
+	.voltage_fail_safe      = 3000,
 	.avail_chg_sources      = AC_CHG | USB_CHG,
 	.batt_technology        = POWER_SUPPLY_TECHNOLOGY_LION,
+	.calculate_capacity     = &msm_calculate_batt_capacity,
 };
+
+static u32 msm_calculate_batt_capacity(u32 current_voltage)
+{
+	u32 low_voltage	 = msm_psy_batt_data.voltage_min_design;
+	u32 high_voltage = msm_psy_batt_data.voltage_max_design;
+
+	if (current_voltage <= low_voltage)
+		return 0;
+	else if (current_voltage >= high_voltage)
+		return 100;
+	else
+		return (current_voltage - low_voltage) * 100
+			/ (high_voltage - low_voltage);
+}
 
 static struct platform_device msm_batt_device = {
 	.name               = "msm-battery",
@@ -886,7 +974,7 @@ static void __init msm7627a_init_regulators(void)
 /*start by lf 2012.12.26 for button-backlight*/
 #if defined(CONFIG_LEDS_GPIO) || defined(CONFIG_LEDS_GPIO_MODULE)
 
-#define I6_LED_GPIO_KEYBOARD  82
+#define I6_LED_GPIO_KEYBOARD  49
 
 static struct gpio_led gpio_leds[] = {
 	[0] = {
@@ -1048,6 +1136,10 @@ static void __init msm_qrd_init(void)
 
 #if defined(CONFIG_BT) && defined(CONFIG_MARIMBA_CORE)
 	msm7627a_bt_power_init();
+#endif
+#ifdef CONFIG_TS_FT5306
+	i2c_register_board_info(1,
+			ft5306_i2c_device, ARRAY_SIZE(ft5306_i2c_device));
 #endif
 	msm7627a_sensor_init();
 	msm7627a_camera_init();
